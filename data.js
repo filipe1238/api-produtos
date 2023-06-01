9e388e6bc62f8f34ac417aa0e683f66eaa2e8561
filipe.correa <filipe.correa@sonner.com.br>
2023-06-01 09:45:07 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,24 @@
+// lista de produtos com id, descricao, preco e marca
+let  produtos = [];
+export default produtos = [
+  { id: 1, descricao: "Mouse", preco: 20.0, marca: "Clone" },
+  { id: 2, descricao: "Teclado", preco: 50.0, marca: "Clone" },
+  { id: 3, descricao: "Monitor", preco: 450.0, marca: "AOC" },
+  { id: 4, descricao: "SSD 256GB", preco: 350.0, marca: "Kingston" },
+  { id: 5, descricao: "SSD 512GB", preco: 620.0, marca: "Kingston" },
+  { id: 6, descricao: "SSD 1TB", preco: 1000.0, marca: "Kingston" },
+  { id: 7, descricao: "Memória 8GB", preco: 350.0, marca: "Kingston" },
+  { id: 8, descricao: "Memória 16GB", preco: 620.0, marca: "Kingston" },
+  { id: 9, descricao: "Memória 32GB", preco: 1000.0, marca: "Kingston" },
+  { id: 10, descricao: "HD 1TB", preco: 350.0, marca: "Seagate" },
+  { id: 11, descricao: "HD 2TB", preco: 620.0, marca: "Seagate" },
+  { id: 12, descricao: "HD 4TB", preco: 1000.0, marca: "Seagate" },
+  { id: 13, descricao: "Placa de Vídeo", preco: 350.0, marca: "EVGA" },
+  { id: 14, descricao: "Placa de Vídeo", preco: 620.0, marca: "EVGA" },
+  { id: 15, descricao: "Placa de Vídeo", preco: 1000.0, marca: "EVGA" },
+  { id: 16, descricao: "Placa de Vídeo", preco: 350.0, marca: "Gigabyte" },
+  { id: 17, descricao: "Placa de Vídeo", preco: 620.0, marca: "Gigabyte" },
+  { id: 18, descricao: "Placa de Vídeo", preco: 1000.0, marca: "Gigabyte" },
+  { id: 19, descricao: "Placa de Vídeo", preco: 350.0, marca: "Asus" },
+  { id: 20, descricao: "Placa de Vídeo", preco: 620.0, marca: "Asus" },
+];
---
+++ server.js
@@ -0,0 +1,57 @@
+import express from "express";
+import data from "./data.js";
+import bodyParser from "body-parser";
+
+const app = express(); // Cria uma instância do Express
+app.use(bodyParser.json());
+
+app.get("/api/produtos", function (req, res) {
+  res.json(data);
+});
+
+app.get("/api/produtos/:id", function (req, res) {
+  const id = req.params.id;
+  const produto = data.find((produto) => produto.id == id);
+  if (produto) {
+    res.json(produto);
+  } else {
+    res.status(404).json({ message: "Produto não encontrado" });
+  }
+});
+
+app.post("/api/produtos", function (req, res) {
+  const produto = req.body;
+  data.push(produto);
+  console.log(data);
+  res.status(201).json(produto);
+});
+
+app.put("/api/produtos/:id", function (req, res) {
+  const id = req.params.id;
+  const produto = req.body;
+  const index = data.findIndex((produto) => produto.id == id);
+  if (index >= 0) {
+    data[index] = produto;
+    res.json(produto);
+  } else {
+    res.status(404).json({ message: "Produto não encontrado" });
+  }
+});
+
+app.delete("/api/produtos/:id", function (req, res) {
+  const id = req.params.id;
+  const index = data.findIndex((produto) => produto.id == id);
+  if (index >= 0) {
+    data.splice(index, 1);
+    res.status(204).end();
+  } else {
+    res.status(404).json({ message: "Produto não encontrado" });
+  }
+});
+
+// Inicializa o servidor HTTP na porta 3000
+const port = process.env.PORT || 3000;
+const server = "127.0.0.1";
+app.listen(port, function () {
+  console.log(`Servidor rodando em http://${server}:${port}`);
+});
